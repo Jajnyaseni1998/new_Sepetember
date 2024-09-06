5d6c2a6cb05cd314e51a1ff568dd1817020b8b0a
Jajnyaseni1998 <jajnyaseniswain123@gmail.com>
2024-09-06 09:44:01 +0530

    first commit

---
+++ src/september/September.java
@@ -0,0 +1,11 @@
+package september;
+
+public class September {
+
+	public static void main(String[] args) {
+		System.out.println("hii i am Jajnaseni");
+		System.out.println("Nice to meet you");
+
+	}
+
+}
